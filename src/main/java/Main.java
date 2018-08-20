35722d973738c489bbeed2de0b742fc3731d951b
andrey <andrey.nagornov@hotmail.com>
2018-08-20 03:14:10 +0300

    Исправлена функция вычисления градиента

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,18 +10,18 @@ import scala.Tuple2;
 public class Main {
     public static void main(String[] args) {
 
-        SparkConf conf = new SparkConf().setAppName("Test").setMaster("local[2]");
+        SparkConf conf = new SparkConf().setAppName("Test").setMaster("local[*]");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        JavaRDD<String> lines = sc.textFile("C:\\Users\\andre\\Desktop\\Gradient-descent-Spark\\src\\main\\java\\sample.txt");
+        JavaRDD<String> lines = sc.textFile("C:\\Users\\andre\\Desktop\\Gradient-descent-Spark\\src\\main\\java\\sample3.txt");
 
         JavaPairRDD<Double[], Double> numbersRDD = lines.mapToPair(
                 s -> {
                     String[] splitString = s.split(" ");
                     Double y = Double.parseDouble(splitString[splitString.length - 1]);
-                    Double[] x = new Double[2];
+                    Double[] x = new Double[1];
 
-                    for (int i =0; i < 2; i++) {
+                    for (int i =0; i < 1; i++) {
                         x[i] = Double.parseDouble(splitString[i]);
                     }
                     return new Tuple2<>(x, y);
@@ -29,18 +29,25 @@ public class Main {
         );
 
         numbersRDD.persist(StorageLevel.MEMORY_ONLY());
-        numbersRDD.collect().forEach(tuple -> { for(Double d : tuple._1()) System.out.print(d + " "); System.out.println(tuple._2()); });
+        //numbersRDD.collect().forEach(tuple -> { for(Double d : tuple._1()) System.out.print(d + " "); System.out.println(tuple._2()); });
 
 
-        System.out.println(numbersRDD.getNumPartitions());
-        numbersRDD.foreach(i -> System.out.println(i));
+        //System.out.println(numbersRDD.getNumPartitions());
+        //numbersRDD.foreach(i -> System.out.println(i));
 
-        //CustumFunction custumFunction = new CustumFunction();
-        //custumFunction.applyThetas(new Double[] {1.0, 1.0});
+        long startTime = System.currentTimeMillis();
 
-        GradientDescent gradientDescent = new GradientDescent();
-        Double[] tt = gradientDescent.calculateGradient(sc, numbersRDD, 2, 4,0.01, 25, 0.01);
+        Double[] tt = GradientDescent.calculateGradient(sc, numbersRDD, 2, 731,0.1, 500, 0.001);
 
+        long timeSpent = System.currentTimeMillis() - startTime;
+
+        System.out.println("Метод выполнялся " + timeSpent + " миллисекунд");
+
+        //Double[] res = GradientDescent.runWithSpark(sc, numbersRDD, 25, 2, 0.01, 10, 0.01);
+
+        for (Double r:tt) {
+            System.out.println(r);
+        }
         System.out.println("Hello World");
 
 
